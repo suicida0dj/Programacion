78af6bc77585b52189d4d9e4055c5d1932edd30f
jguerreroa <nnadie030@gmail.com>
2022-01-14 19:21:36 +0100

    EJERCICIO 1 CASI ACABADO NECESARIO (ARREGLAR EL MATH RANDOM)

--- EjerciciosOO/src/Main.java
+++ EjerciciosOO/src/Main.java
@@ -5,7 +5,22 @@ public class Main {
         return v == 'H';
     }
 
-    public static void main(String[] args) {
+    public static String comprobarPesoIdeal(Persona persona){
+        if (persona.calcularIMC() == -1){
+            return "Estas por debajo del peso recomendado";
+        }else if (persona.calcularIMC() == 0){
+            return "Estas en tu peso recomendado";
+        }else{
+            return "Estas por encima de tu peso recomendado";
+        }
+    }
+
+    public static String comprobarEdad(Persona persona) {
+        if (persona.getEdad()>18) return "Es mayor de 18";
+        return "no es mayor de 18";
+    }
+
+        public static void main(String[] args) {
         int edad;
         String nombre;
         float peso, altura;
@@ -35,9 +50,28 @@ public class Main {
             persona3.setSexo(Persona.Sexo.M);
         }
 
+        //Peso ideal
+        System.out.println(comprobarPesoIdeal(persona1));
 
+        System.out.println(comprobarPesoIdeal(persona2));
 
+        System.out.println(comprobarPesoIdeal(persona3));
 
+        //Mayor de edad
+
+        System.out.println(comprobarEdad(persona1));
+
+        System.out.println(comprobarEdad(persona2));
+
+        System.out.println(comprobarEdad(persona3));
+
+        //mostrar informacion
+
+        System.out.println(persona1.toString());
+
+        System.out.println(persona2.toString());
+
+        System.out.println(persona3.toString());
     }
 
 
